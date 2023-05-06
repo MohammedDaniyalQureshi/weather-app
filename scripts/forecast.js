88cfb97b96bb03668562cdfa66419fe35583d056
Mohammed Daniyal Qureshi <daniyalqureshi786@gmail.com>
2023-05-06 21:55:53 +0530

    Weather App

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = '3fON9JCchGRcQIddnc4Rho6mgLi0DHeH';
+
+
+// get weather information
+const getWeather = async (id) => {
+  
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
